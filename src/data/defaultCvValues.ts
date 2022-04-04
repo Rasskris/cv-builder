311e4c781b293b3ca235464c4023e4584216893d
Rasskris <rassoshenko.kristina@yandex.ru>
2022-04-04 15:07:47 +0300

    feat: add experience info to deafult cv values

--- src/data/defaultCvValues.ts
+++ src/data/defaultCvValues.ts
@@ -1,23 +1,35 @@
-import { Cv } from '../types';
+import uniqueId from 'lodash/uniqueId';
 
-const skillsSet = new Set([
-  'TypeScript',
-  'JavaScript',
-  'React',
-  'Redux',
-  'MobX',
-  'Next',
-  'Express',
-  'Nest',
-  'MongoDB',
-  'TypeORM',
-  'Socket-io',
-  'Sass',
-  'Jest',
-  'Material-UI',
-  'Sequelize',
-  'PostgreSQL',
-]);
+import { Cv, Experience } from '../types';
+
+const skills = [
+  { id: uniqueId(), name: 'TypeScript' },
+  { id: uniqueId(), name: 'JavaScript' },
+  { id: uniqueId(), name: 'React' },
+  { id: uniqueId(), name: 'Redux' },
+  { id: uniqueId(), name: 'MobX' },
+  { id: uniqueId(), name: 'Next' },
+  { id: uniqueId(), name: 'Express' },
+  { id: uniqueId(), name: 'Nest' },
+  { id: uniqueId(), name: 'MongoDB' },
+  { id: uniqueId(), name: 'TypeORM' },
+  { id: uniqueId(), name: 'Socket-io' },
+  { id: uniqueId(), name: 'Sass' },
+  { id: uniqueId(), name: 'Jest' },
+  { id: uniqueId(), name: 'Material-UI' },
+  { id: uniqueId(), name: 'Sequelize' },
+  { id: uniqueId(), name: 'PostgreSQL' },
+];
+
+const experience: Experience[] = [
+  {
+    id: uniqueId(),
+    jobTitle: 'Front-end developer',
+    employer: 'Epam',
+    startEndDate: [null, null],
+    description: 'Building stable and maintainable codebases using React',
+  },
+];
 
 export const DEFAULT_CV_VALUES: Cv = {
   firstName: 'Kristina',
@@ -26,5 +38,6 @@ export const DEFAULT_CV_VALUES: Cv = {
     'Frontend Developer is seeking an opportunity to extend my web development skills and knowledge',
   phone: '8-987-779-88-34',
   email: 'rs.kris@gmail.com',
-  skills: skillsSet,
+  skillList: skills,
+  experienceList: experience,
 };
